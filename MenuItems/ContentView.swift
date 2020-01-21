9a62473829ef24d3b2f0b69d532c8cf0aa21d6c9
Damiaan Dufaux <damiaan@dpro.be>
2020-01-21 22:03:47 +0100

    Add MenuButton

--- MenuItems/ContentView.swift
+++ MenuItems/ContentView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+		MenuButton("Show menu") {
+			Button("first") {}
+			Button("second") {}
+			Divider()
+			Text("label")
+			Button("third") {}
+		}
     }
 }
 
